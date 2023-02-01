d593be260a44665beba3c23f897752b6d51f2e9f
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-02-01 17:22:14 +0530

    completed email verification

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+let emailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+
+
+function emailVarification(){
+    let emailID = document.getElementById('emailID');
+    let emailerrmsg = document.getElementById('emailerrmsg');
+    let emailwrapper = document.getElementById('emailwrapper');
+    let erroricon = document.getElementById('erroricon');
+    let btnprimary = document.getElementById('btn-primary');
+
+    if(!emailID.value.match(emailRegex)){
+        emailerrmsg.classList.remove(['invisible']);
+        emailerrmsg.classList.add(['visible']);
+        emailID.classList.remove(['border-dark']);
+        emailwrapper.classList.remove(['border-primary']);
+        emailwrapper.classList.add(['border-danger']);
+        erroricon.classList.remove(['d-none']);
+        erroricon.classList.add(['d-block']);
+        btnprimary.classList.remove(['errorbtn']);
+        btnprimary.classList.add(['errorTrue']);
+    }
+    else{
+        emailerrmsg.classList.remove(['d-block']);
+        emailerrmsg.classList.add(['d-none']);
+        // erroricon.classList.add(['d-none']);
+        // erroricon.classList.remove(['d-block']);
+        emailwrapper.style.border = '1px solid black'
+    }
+}
